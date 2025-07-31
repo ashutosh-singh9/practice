8a827d6927ec82d02161311cb504f9fb367cb0dd
Ashutosh Singh <singhashutosh4123@gmail.com>
2025-07-31 14:50:33 +0000

    studied for loops

---
+++ for loops/app.js
@@ -0,0 +1,31 @@
+//basic for loop
+
+// for (let i=1;i<=5;i++){
+//     console.log(i);
+// }
+
+// Print all odd numbers(1 to 15)
+
+// console.log("All odd numbers (1 to 15)");
+// for (let i = 1; i <= 15; i = i + 2) {
+//   console.log(i);
+// }
+
+//Print all even numbers(1 to 15)
+// for (let i=2; i <= 15; i+=2){
+//     console.log(i);
+// }
+
+// Print the multiplication for 5
+// let n = prompt("Write ur number")
+
+// for (i=1; i<=10;i++){
+//     console.log(`${n} x ${i} = ${n*i}`)
+// }
+
+//nested for loops
+for(i=1;i<=3;i++){
+    for(j=1;j<=3;j++){
+        console.log(j)
+    }
+}
