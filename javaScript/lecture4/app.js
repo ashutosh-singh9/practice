a388ffce0c8e757a736c79316744663e93d863c0
Ashutosh Singh <singhashutosh4123@gmail.com>
2025-08-17 09:34:52 +0000

    lecture 4 added

---
+++ javaScript/lecture4/app.js
@@ -0,0 +1,96 @@
+// let i = 2;
+// while (i<=20){
+//     console.log(i);
+//     i=i+2;
+// }
+
+// let i = 2;
+// while(i <=20 ){
+//     console.log(i);
+//     i +=2
+// }
+
+// let n = prompt("Enter the number");
+// let i = 1;
+// while (i<=10) {
+//     console.log(`${n} X ${i} = ${n*i}`);
+//     i++;
+// }
+
+//mine methods
+
+// guess favorite movie!!
+// let fav_mov = "kgf";
+// let inp = prompt("enter the guess!!");
+// if (fav_mov == inp) {
+//   console.log("You guessed it right!!");
+// } else if (inp == "quit") {
+//   console.log("Quiting");
+// }
+// // } else if (fav_mov != inp) {
+//   while ((fav_mov == inp)||(inp=="quit")) {
+//     inp = prompt("You guessed it wrong");
+//   }
+
+//   if (fav_mov != inp){
+// 	console.log("You guessed it wrong")
+//   }
+// }
+
+//  }
+
+//mam's method
+// const favMov = "kgf";
+// let guess = prompt("Guess the movie!");
+
+// while ((guess != favMov) && (guess !="quit")){
+//     guess = prompt("You guessed the wrong movie, please try again later");
+// }
+
+// if(guess == favMov){
+//     console.log("You guessed it right!!");
+// }
+
+// else{
+//     console.log("Quited");
+// }
+
+// let i =1;
+// while (i <= 5) {
+//   if (i == 3) {
+//     break;
+//   }
+//   console.log(i);
+//   i++;
+// }
+
+// let fruits = ["Mango","Banana","Apple","litchi","orange"]
+// for(i=0;i<fruits.length;i++){
+//     console.log(i, fruits[i]);
+// }
+
+// for(let i=fruits.length-1 ; i>=0; i--){
+//     console.log(i,fruits[i]);
+// }
+
+// let hero = [["Captain America","Ironman","Thor","Hulk"],["Superman","Flash","Batman","wonder women"]]
+// for(i=0;i<hero.length;i++){
+//     console.log(i)
+// for(j=0;j<hero[i].length;j++){
+//     console.log(hero[i][j])
+// }
+// }
+
+//for of loop
+// let fruits = ["Mango","Banana","Apple","litchi","orange"]
+// for(fruit of fruits){
+//     console.log(fruit);
+// }
+
+// let heroes = [["Captain America","Ironman","Thor","Hulk"],["Superman","Flash","Batman","wonder women"]];
+// for(group of heroes){
+//     for(hero of group){
+//         console.log(hero);
+//     }
+
+// }
