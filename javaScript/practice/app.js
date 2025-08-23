a351c34c1e61eb61c2d6315b4a16c17eaaa78de6
Ashutosh Singh <singhashutosh4123@gmail.com>
2025-08-23 14:45:39 +0000

    updated something!

--- javaScript/practice/app.js
+++ javaScript/practice/app.js
@@ -6,7 +6,22 @@
 //     console.log(word[i]);
 // };
 
-let arr = ["Ashu","Krishna","Rohan","Rishi","Sakshi"];
+// let arr = ["Ashu","Krishna","Rohan","Rishi","Sakshi"];
 // let revArr = arr.reverse()
 // let sliceArr = revArr.slice(2,4)
 
+// let users = [
+// { name: "Ashu", age: 19 },
+// { name: "Zoya", age: 22 },
+// { name: "Bob", age: 18 }
+// ];
+
+// // Sort by age
+// users.sort((a, b) => a.age - b.age);
+// console.log(users);
+
+// Sort by name
+// users.sort((a, b) => a.name.localeCompare(b.name));
+// console.log(users);
+
+let arr = ["Ashu","Krishna","Rohan","Rishi","Sakshi"];
